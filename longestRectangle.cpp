4c231758b1be7c20a774f4ded0c3e4570a676693
shank <shashaankjain07@gmail.com>
2026-03-31 13:01:46 +0530

    Added solution for longest rectangle in histogram

---
+++ longestRectangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int maxArea = 0;
+        int n = heights.size();
+        stack<int>st;
+        for(int i=0;i<=n;i++){
+            int currentHeight = (i==n)? 0 :heights[i];
+
+            while(!st.empty() && currentHeight<heights[st.top()]){
+                int h = heights[st.top()];
+                st.pop();
+                int w = st.empty()?i:i-st.top()-1;
+                maxArea = max(maxArea,h*w);
+            }
+
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};
