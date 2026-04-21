4c149cd6c8ab40bbe568f4eed00d466a2e5715f8
shank <shashaankjain07@gmail.com>
2026-04-21 11:39:24 +0530

    Added solution for non overlapping intervals

---
+++ nonOverlap.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(), [](const vector<int>&a, const vector<int>&b){
+            return a[1]<b[1];
+        });
+        
+        int count = 0;
+        int last_end = intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<last_end){
+                count++;
+            }else{
+                last_end = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+};
