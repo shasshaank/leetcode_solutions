5ead273108851d6d53a2eb76328b813ed7d72fbe
shank <shashaankjain07@gmail.com>
2026-08-13 17:33:01 +0530

    Added Implementation and advanced operations on tries

---
+++ Trie.cpp
@@ -0,0 +1,110 @@
+struct Node{
+    Node* links[26] = {nullptr};
+    int cntEndWith = 0;
+    int cntPrefix = 0;
+
+
+    bool containsKey(char ch){
+        return (links[ch-'a'] != nullptr);
+    }
+
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+
+    void put(char ch, Node* node){
+        links[ch-'a'] = node;
+    }
+
+    void increaseEnd(){
+        cntEndWith++;
+    }
+
+    void increasePrefix(){
+        cntPrefix++;
+    }
+
+    void decreasePrefix(){
+        cntPrefix--;
+    }
+
+    void deleteEnd(){
+        cntEndWith--;
+    }
+
+    int getPrefix(){
+        return cntPrefix;
+    }
+
+    int getEnd(){
+        return cntEndWith;
+    }
+
+
+};
+
+class Trie {
+    Node* root;
+	public:
+	    Trie() {
+	        root = new Node();
+	    }
+
+	    void insert(string word) {
+	        Node* node = root;
+            for(int i=0;i<word.size();i++){
+                if(!node->containsKey(word[i])){
+                    node->put(word[i],new Node());
+                }
+                node = node->get(word[i]);
+                node->increasePrefix();
+            }
+            node->increaseEnd();
+    	}
+
+	    int countWordsEqualTo(string word) {
+	        Node* node = root;
+            for(int i=0;i<word.size();i++){
+                if(node->containsKey(word[i])){
+                    node = node->get(word[i]);
+                }else{
+                    return 0;
+                }
+            }
+            return node->getEnd();
+    	}
+
+	    int countWordsStartingWith(string word) {
+	        Node* node = root;
+            for(int i=0;i<word.size();i++){
+                if(node->containsKey(word[i])){
+                    node = node->get(word[i]);
+                }else{
+                    return 0;
+                }
+            }
+            return node->getPrefix();
+    	}
+
+	    void erase(string word) {
+	        Node* node = root;
+            for(int i=0;i<word.size();i++){
+                if(node->containsKey(word[i])){
+                    node = node->get(word[i]);
+                    node->decreasePrefix();
+                }else{
+                    return;
+                }
+            }
+            node->deleteEnd();
+	    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * int param_2 = obj->countWordsEqualTo(word);
+ * int param_3 = obj->countWordsStartingWith(prefix);
+ * obj->erase(word);
+ */
